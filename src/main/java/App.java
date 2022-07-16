a775a7ea78a6aff7dc63b7cb0c7505819fd1a0b1
joong2043 <tbvjqlqlqlr@gmail.com>
2022-07-16 20:49:09 +0900

    WiseSaying 클래스 생성, App 클래스에 WiseSaying 어레이리스트 생성해서 등록 기능 구현

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,9 +1,41 @@
-public class App {
-    App(){
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class App {
+    Scanner sc;
+    int lastIndex;
+
+    ArrayList<WiseSaying> wiseSayings;
+
+    App(){
+        sc = new Scanner(System.in);
+        lastIndex = -1;
+        wiseSayings = new ArrayList<WiseSaying>();
     }
 
     void run(){
 
+        outer:
+        while(true){
+            System.out.printf("명령) ");
+            String cmd = sc.nextLine().trim();
+
+            switch (cmd){
+                case "등록":
+                    lastIndex++;
+                    System.out.printf("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.printf("작가 : ");
+                    String author = sc.nextLine().trim();
+                    WiseSaying wiseSaying = new WiseSaying(lastIndex,content,author);
+
+                    wiseSayings.add(wiseSaying);
+
+                    break;
+
+                case "종료":
+                    break outer;
+            }
+        }
     }
 }
